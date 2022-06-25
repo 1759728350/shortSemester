b6a9cc90508053f38463409fc1c72d7a9555ac31
Hedley <1759728350@qq.com>
2022-06-25 15:40:15 +0800

    login finished

--- springboot_crs/src/main/java/com/example/springboot_crs/controller/UserController.java
+++ springboot_crs/src/main/java/com/example/springboot_crs/controller/UserController.java
@@ -1,61 +1,53 @@
 package com.example.springboot_crs.controller;
 
+import cn.hutool.core.util.StrUtil;
 import com.example.springboot_crs.entity.User;
 import com.example.springboot_crs.service.UserService;
+import com.example.springboot_crs.vo.ErrorCode;
+import com.example.springboot_crs.vo.Result;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
+
 
 @RestController
 @RequestMapping("/user")
-//@CrossOrigin解决前端跨域问题
 @CrossOrigin
 public class UserController {
 
-    /*
-    * author:李梓瑀
-    * data:2022/6/22
-    */
+    @Autowired
+    private UserService userService;
 
-    @Autowired(required = false)
-    private UserService service;
+    /**
+     * @description:
+     * @param: [userVo]
+     * @return: com.example.springboot_crs.entity.User
+     * @author Hedley
+     * @date: 2022-06-24 14:56
+     */
+    @GetMapping("/login")
+    public Result login(@RequestParam("userAccount") String account,
+                        @RequestParam("userPassword") String password) {
 
-    //查询所有用户
-    @GetMapping("/findAll")
-    public List<User> selectAll(){
-        System.out.println("查询所有");
-        return service.selectAllUser();
+        //判断账号密码是否为空
+        if (StrUtil.isBlank(account) || StrUtil.isBlank(password)) {
+
+            return Result.fail(ErrorCode.PARAMS_ERROR.getCode(),
+                    ErrorCode.PARAMS_ERROR.getMsg());
+        }
+        List<User> userList = userService.selectUserByAccount(account);
+
+        for (User user : userList) {
+
+            if (user.getUserPassword().equals(password)) {
+                return Result.success(user);
+            }
+
+        }
+        //null返回结果是1,因此用Result返回解决
+        return Result.fail(ErrorCode. QUERY_RESULT_IS_EMPTY.getCode(),
+                ErrorCode. QUERY_RESULT_IS_EMPTY.getMsg());
     }
-
-    //新增及修改新用户
-    @PostMapping("/addUser")
-    public Integer save(@RequestBody User user){
-        System.out.println("新增用户");
-        return service.addNewUser(user);
-    }
-
-    //更新用户
-    @PutMapping("/updateUser")
-    public Integer update(@RequestBody User user){
-        System.out.println("更新用户");
-        return service.updateUser(user);
-    }
-
-    //删除用户
-    @DeleteMapping("/deleteUser/{id}")
-    public Integer delete(@PathVariable("id") String id){
-        System.out.println("删除用户");
-        System.out.println("id为"+id);
-        return service.deleteUser(id);
-    }
-
-    //按照用户账号和密码查找用户
-    @GetMapping("/findUserByAccount")
-    public User loginByAccount(@RequestParam("account") String account,@RequestParam("password") String password){
-        System.out.println("通过账号密码查找用户");
-        return service.selectUserByAccount(account,password);
-    }
-
 
 }
--- springboot_crs/src/main/java/com/example/springboot_crs/service/UserService.java
+++ springboot_crs/src/main/java/com/example/springboot_crs/service/UserService.java
@@ -10,36 +10,12 @@ import java.util.List;
 @Service
 public class UserService {
 
-    /*
-     * author:李梓瑀
-     * data:2022/6/22
-     */
 
-    @Autowired(required = false)
+
+    @Autowired
     private UserMapper userMapper;
-    //查询所有
-    public List<User> selectAllUser(){
-        List<User> list = userMapper.findAll();
-        return list;
-    }
 
-    //新增
-    public Integer addNewUser(User user) {
-        return userMapper.addUser(user);
-    }
-
-    //更新
-    public Integer updateUser(User user) {
-        return userMapper.update(user);
-    }
-
-    //删除
-    public Integer deleteUser(String id) {
-        return userMapper.deleteUser(id);
-    }
-
-    //通过账号密码查找
-    public User selectUserByAccount(String account,String password) {
-        return userMapper.findByAccount(account,password);
+    public List<User> selectUserByAccount(String account) {
+        return userMapper.selectUserByAccount(account);
     }
 }
